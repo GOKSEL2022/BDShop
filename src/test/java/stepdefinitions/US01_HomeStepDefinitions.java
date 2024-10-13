83dd781f5bdafdd974db9b3a8b4d8782db037181
Göksel Celik <gokselcelik5255@gmail.com>
2024-10-14 00:34:00 +0300

    14.10.24

--- src/test/java/stepdefinitions/US01_HomeStepDefinitions.java
+++ src/test/java/stepdefinitions/US01_HomeStepDefinitions.java
@@ -1,24 +1,21 @@
 package stepdefinitions;
-
 import io.cucumber.java.en.*;
+import org.testng.asserts.SoftAssert;
 import pages.AllPages;
 import utilies.ConfigReader;
 import utilies.Driver;
-import utilies.ReusableMethods;
-
 import static utilies.ReusableMethods.clickWithJS;
-
 public class US01_HomeStepDefinitions {
     AllPages pages=new AllPages();
+    SoftAssert softAssert=new SoftAssert();
     @Given("kullanici url ye gider")
     public void kullaniciUrlYeGider() {
         Driver.getDriver().get(ConfigReader.getProperty("url"));
     }
     @When("kullanici anasayfanin acildigini dogrular")
     public void kullaniciAnasayfaninAcildiginiDogrular() {
-        assert Driver.getDriver().getTitle().contains("BD SHOP | Online Shopping BD | YouTube Gadget & Gear Shop");
+        assert Driver.getDriver().getTitle().equalsIgnoreCase("BD SHOP | Online Shopping BD | YouTube Gadget & Gear Shop");
     }
-
     @And("kullanici loginHome butona tiklar")
     public void kullaniciLoginHomeButonaTiklar() {
         clickWithJS(pages.homePage().loginButonHome);
@@ -29,8 +26,9 @@ public class US01_HomeStepDefinitions {
     }
     @And("kullanici email ve password alanlarinda This is a required field uyarisini goruntuler")
     public void kullaniciEmailVePasswordAlanlarindaThisIsARequiredFieldUyarisiniGoruntuler() {
-        assert pages.loginPage().errorMessageThisIsARequiredFiel1dLogin.isDisplayed();
-        assert pages.loginPage().errorMessageThisIsARequiredFiel2dLogin.isDisplayed();
+        softAssert.assertTrue(pages.loginPage().errorMessageThisIsARequiredFiel1dLogin.isDisplayed());
+        softAssert.assertTrue(pages.loginPage().errorMessageThisIsARequiredFiel2dLogin.isDisplayed());
+        softAssert.assertAll();
     }
     @And("kullanici sayfayi kapatir")
     public void kullaniciSayfayiKapatir() {
